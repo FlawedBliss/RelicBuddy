ed47d5ef19c05461aff4b917cc942bd37f5f1273
FlawedBliss <flawedbliss@trashprojects.moe>
2026-02-11 15:14:53 +0100

    fix lingering merge issue

--- RelicBuddy/Plugin.cs
+++ RelicBuddy/Plugin.cs
@@ -46,9 +46,6 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService]
     internal static IObjectTable ObjectTable { get; private set; } = null!;
     
-    [PluginService]
-    internal static IObjectTable ObjectTable { get; private set; } = null!;
-    
     private const string RelicBuddyCommandName = "/rb";
     private const string RelicBuddyDebugCommandName = "/rbd";
 
